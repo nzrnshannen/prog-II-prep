3dd1495e1509b76824f672606fec46c62dad6888
Shannen Nazareno <nzrnshannen@gmail.com>
2023-01-12 20:34:39 +0800

    added [80]

---
+++ [80] Number of distinct pairs.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 50
+
+struct node{
+	int data;
+	struct node *next;
+};
+
+struct node *head, *temp, *newNode, *temp1, *temp2;
+
+int main()
+{
+	head=NULL;
+	int i=1, n, pairCount=0, diff;
+	printf("Input the number of nodes for your linked list: ");
+	scanf("%d", &n);
+	
+	while(i<=n)
+	{
+		newNode=(struct node*)malloc(sizeof(struct node));
+		printf("Data %d: ", i++);
+		scanf("%d", &newNode->data);
+		newNode->next=NULL;
+		
+		if(head==NULL)
+		{
+			head=temp=newNode;
+		}
+		else
+		{
+			temp->next=newNode;
+			temp=newNode;
+		}
+	}
+	
+	
+	printf("\nThe created linked list: ");
+	temp=head;
+	while(temp!=NULL)
+	{
+		printf("%d ", temp->data);
+		temp=temp->next;
+	}
+	
+	int flag=0;
+	
+	printf("\n\nInput the difference: ");
+	scanf("%d", &diff);
+	
+	printf("\n\nThe distinct pair/s for difference %d is/are:\n", diff);
+	for(temp1=head; temp1!=NULL; temp1=temp1->next)
+	{
+		for(temp2=head; temp2!=NULL; temp2=temp2->next)
+		{
+			if((temp1->data - temp2->data)==diff)
+			{
+				flag=1;
+				pairCount++;
+				printf("[%d, %d]\n", temp1->data, temp2->data);
+				
+			}
+		}
+	}
+	
+	if(flag==0)
+	{
+		printf("Number of distinct pairs: %d\n", pairCount);
+		printf("\n\nNo distinct pairs were found in the list.\n");
+	}
+	else
+		printf("\n\nNumber of distinct pairs: %d\n", pairCount);
+	
+	head=temp=temp1=temp2=newNode=NULL;
+	free(head);
+	free(temp);
+	free(temp1);
+	free(temp2);
+	free(newNode);
+	
+	return 0;
+	
+}
