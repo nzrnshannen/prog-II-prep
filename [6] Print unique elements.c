aa10b899a0e63a7b5529e5a529f16b3139119074
Shannen Nazareno <nzrnshannen@gmail.com>
2023-01-15 19:03:17 +0800

    added modifications to [6]

--- [6] Print unique elements.c
+++ [6] Print unique elements.c
@@ -6,60 +6,59 @@ struct node{
 	struct node *next;
 };
 
-struct node *head, *temp, *newNode, *temp1, *temp2;
+struct node *head, *temp, *temp1, *temp2, *newNode;
 
 int main()
 {
 	head=NULL;
-	int i=1, j, n;
-	printf ("Input the number of nodes for your linked list: ");
+	int i=1, n;
+	printf("Input the number of nodes for your linked list: ");
 	scanf("%d", &n);
 	
-	printf ("Input %d data for your linked list:\n", n);
+	printf("Input %d data:\n", n);
 	while(i<=n)
 	{
 		newNode=(struct node*)malloc(sizeof(struct node));
-		printf ("Data %d: ", i);
+		printf("Data %d: ", i++);
 		scanf("%d", &newNode->data);
 		newNode->next=NULL;
 		
 		if(head==NULL)
-		{
 			head=temp=newNode;
-		}
 		else
 		{
 			temp->next=newNode;
 			temp=newNode;
 		}
-		
-		i++;
 	}
 	
+	printf("\n\nThe created linked list: ");
 	temp=head;
 	
-	int flag=0;
+	while(temp!=NULL)
+	{
+		printf("%d ", temp->data);
+		temp=temp->next;
+	}
+	
+	int flag;
+	
+	printf("\n\nThe unique numbers are as follows: ");
 	
 	for(temp1=head; temp1!=NULL; temp1=temp1->next)
 	{
-		if(temp1->data==-1)
-		{
+		if(temp1->data==-9999)
 			continue;
-		}
 		
 		flag=0;
+		
 		for(temp2=temp1->next; temp2!=NULL; temp2=temp2->next)
 		{
-			if(temp1->data == temp2->data && flag==0 && temp2->data!=-1 && temp1->data!=-1)
+			if(temp1->data == temp2->data && temp2->data!=-9999)
 			{
 				flag=1;
-				temp2->data=-1;
-				continue;
-			}
-			else if(flag==1 && temp1->data == temp2->data && temp2->data!=-1)
-			{
-				temp2->data=-1;
-			}
+				temp2->data=-9999;
+			}	
 		}
 		
 		if(flag==0)
@@ -68,12 +67,17 @@ int main()
 		}
 	}
 	
+	if(flag==1)
+	{
+		printf("\nSorry, there are no unique numbers in the list.\n\n");
+	}
+	
+	head=temp=temp1=temp2=newNode=NULL;
 	free(head);
-	free(newNode);
 	free(temp);
 	free(temp1);
 	free(temp2);
+	free(newNode);
 	
 	return 0;
-
 }
