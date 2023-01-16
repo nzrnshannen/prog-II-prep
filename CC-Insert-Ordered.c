26d043b88c6a9f668f45ea21c18c3b181ebbf0ce
Shannen Nazareno <nzrnshannen@gmail.com>
2023-01-16 21:09:53 +0800

    modified CC-Insert-Ordered

--- CC-Insert-Ordered.c
+++ CC-Insert-Ordered.c
@@ -2,68 +2,66 @@
 #include <stdlib.h>
 
 struct node{
-	int num;
+	int data;
 	struct node *next;
-};
-
-struct node *head, *temp, *temp1, *temp2, *newNode, *traverse;
-int n;
+}*head, *temp, *temp1, *temp2, *newNode, *traverse;
 
 void sortList();
 
 int main()
 {
 	head=NULL;
+	int diff=0;
 	int data;
-	
-	inputAgain:
+
+inputAnother:	
+	newNode=(struct node*)malloc(sizeof(struct node));
 	printf("Insert Data Value: ");
 	scanf("%d", &data);
+	
+	if(data!=0)
+	{
+		newNode->data=data;
+		newNode->next=NULL;
 		
-	if(data==0)
+		if(head==NULL)
+		{
+			head=temp=newNode;
+		}
+		else
+		{
+			temp->next=newNode;
+			temp=newNode;
+		}
+		
+		sortList();
+		
+		printf("Linked list: ");
+		traverse=head;
+		printf("\n");
+		while(traverse!=NULL)
+		{
+			printf("%d ", traverse->data);
+			traverse=traverse->next;
+		}
+		
+		printf("\n\n");
+		goto inputAnother;
+		
+	}
+	else
 	{
 		goto end;
 	}
 	
-	newNode=(struct node*)malloc(sizeof(struct node));
-	newNode->num = data;
-	newNode->next=NULL;
-		
-	if(head==NULL)
-	{
-		head=temp=newNode;
-	}
-	else
-	{
-		temp->next=newNode;
-		temp=newNode;
-	}
-		
-	sortList();
-		
-	traverse=head;
-	printf("LinkedList Data:\n");
-	while(traverse!=NULL)
-	{
-		printf("%d ", traverse->num);
-		traverse=traverse->next;
-	}
-	
-	printf("\n\n");
-		
-	if(data!=0)
-	{
-		goto inputAgain;
-	}
-	
 	
 	end:
-	head=temp=temp1=temp2=traverse=newNode=NULL;
+	head=traverse=temp=temp1=temp2=newNode=NULL;
 	free(head);
+	free(traverse);
 	free(temp);
 	free(temp1);
 	free(temp2);
-	free(traverse);
 	free(newNode);
 	
 	return 0;
@@ -71,19 +69,17 @@ int main()
 
 void sortList()
 {
+	int tmpNum;
 	for(temp1=head; temp1->next!=NULL; temp1=temp1->next)
 	{
 		for(temp2=head; temp2->next!=NULL; temp2=temp2->next)
 		{
-			int t;
-			if(temp2->num > temp2->next->num)
+			if(temp2->data > temp2->next->data)
 			{
-				t=temp2->num;
-				temp2->num=temp2->next->num;
-				temp2->next->num=t;
+				tmpNum=temp2->data;
+				temp2->data = temp2->next->data;
+				temp2->next->data=tmpNum;
 			}
 		}
 	}
-	
-	return;
 }
