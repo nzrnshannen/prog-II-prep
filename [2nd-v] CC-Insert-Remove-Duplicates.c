78e528422210ce1d3fe26833576a52e6631ad79f
Shannen Nazareno <nzrnshannen@gmail.com>
2023-01-16 21:24:13 +0800

    added the 2nd version of insert-remove-duplicates

---
+++ [2nd-v] CC-Insert-Remove-Duplicates.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+	int data;
+	struct node *next;
+}*head, *temp, *newNode, *check;
+
+int main()
+{
+	head=NULL;
+	int n, i=1;
+
+inputNo:
+	printf("Enter the number of nodes for your linked list: ");
+	scanf("%d", &n);
+	
+	if(n<=0)
+	{
+		printf("\n\nInput does not create a linked list. Try a different one.\n\n");
+		goto inputNo;
+	}
+	
+	while(i<=n)
+	{
+		newNode=(struct node*)malloc(sizeof(struct node));
+		printf("Input Value %d: ", i++);
+		scanf("%d", &newNode->data);
+		newNode->next=NULL;
+		
+		if(head==NULL)
+		{
+			head=temp=newNode;
+		}
+		else
+		{
+			int flag=0;
+			
+			check=head;
+			
+			while(check!=NULL)
+			{
+				if(check->data == newNode->data)
+				{
+					flag=1;
+				}
+				
+				check=check->next;
+			}
+			
+			if(flag==1)
+			{
+				continue;
+			}
+			else
+			{
+				temp->next=newNode;
+				temp=newNode;
+			}
+		}
+		
+	}
+	
+	printf("\nResult:\n");
+	temp=head;
+	
+	while(temp!=NULL)
+	{
+		printf("%d ", temp->data);
+		temp=temp->next;
+	}
+	
+	head=temp=check=newNode=NULL;
+	free(head);
+	free(temp);
+	free(check);
+	free(newNode);
+	
+	return 0;
+}
+
